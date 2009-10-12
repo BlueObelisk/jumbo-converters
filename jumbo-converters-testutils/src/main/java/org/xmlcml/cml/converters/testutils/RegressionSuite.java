98a5aec8a7aa454a4415b53529dc1e5d5f9088e0
Jim Downing <jimdowning@pobox.com>
2009-10-12 12:11:33 +0100

    deleted unneeded lines from RegressionSuite

--- jumbo-converters-testutils/src/main/java/org/xmlcml/cml/converters/testutils/RegressionSuite.java
+++ jumbo-converters-testutils/src/main/java/org/xmlcml/cml/converters/testutils/RegressionSuite.java
@@ -1,16 +1,12 @@
 package org.xmlcml.cml.converters.testutils;
 
-import java.util.logging.Level;
 import static org.xmlcml.euclid.EuclidConstants.S_WHITEREGEX;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileReader;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import nu.xom.Builder;
 import nu.xom.Element;
@@ -21,10 +17,8 @@ import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.xmlcml.cml.base.CMLConstants;
-import org.xmlcml.cml.converters.AbstractConverter;
 import org.xmlcml.cml.converters.Converter;
 import org.xmlcml.cml.testutil.TestUtils;
-import org.xmlcml.cml.converters.Type;
 
 public class RegressionSuite {
 
@@ -32,151 +26,11 @@ public class RegressionSuite {
    static String OUTPUT_DIR_ROOT_NAME = "target/test";
    private final static File TEST_RESOURCES = new File("src/test/resources");
    static String DIRECTORY = "_DIR";
-   private File outputDir;
-   private String startDirName;
    protected String localDirName;
-   private String referenceDirName;
    private String inputSuffix;
    private String outputSuffix;
-   private String converterName;
-   private String auxiliaryFileName;
-   private boolean verbose = false;
-   private boolean quiet = false;
-   private Type inputType;
-   private Type outputType;
    private Converter converter;
-   private String testDir;
-   private String testRoot;
-//
-//   public void setVerbose(boolean verbose) {
-//      this.verbose = verbose;
-//   }
-//
-//   public void setQuiet(boolean quiet) {
-//      this.quiet = quiet;
-//   }
-//
-//   public void setConverterName(String className) {
-//      this.converterName = className;
-//   }
-//
-//   public File getOutputDir() {
-//      return outputDir;
-//   }
-//
-//   public void setOutputDir(File outputDir) {
-//      this.outputDir = outputDir;
-//   }
-//
-//   public Type getInputType() {
-//      return inputType;
-//   }
-//
-//   public void setInputType(Type inputType) {
-//      this.inputType = inputType;
-//   }
-//
-//   public Type getOutputType() {
-//      return outputType;
-//   }
-//
-//   public void setOutputType(Type outputType) {
-//      this.outputType = outputType;
-//   }
-//
-//   public AbstractConverter getConverterInstance() {
-//      return converterInstance;
-//   }
-//
-//   public void setConverterInstance(AbstractConverter converterInstance) {
-//      this.converterInstance = converterInstance;
-//   }
-//
-//   public String getTestDir() {
-//      return testDir;
-//   }
-//
-//   public void setTestDir(String testDir) {
-//      this.testDir = testDir;
-//   }
-//
-//   public String getConverterName() {
-//      return converterName;
-//   }
-//
-//   public void setInputSuffix(String inputSuffix) {
-//      this.inputSuffix = inputSuffix;
-//   }
-//
-//   public void setOutputSuffix(String outputSuffix) {
-//      this.outputSuffix = outputSuffix;
-//   }
-//
-//   public void setAuxiliaryFileName(String auxiliaryFileName) {
-//      this.auxiliaryFileName = auxiliaryFileName;
-//   }
-//
-//   public void setTestRoot(String testRoot) {
-//      this.testRoot = testRoot;
-//   }
-//
-//   public String getTestRoot() {
-//      return testRoot;
-//   }
-//
 
-//   /**
-//    * @param c
-//    * @param outputDir
-//    */
-//   public void testDirectories(String[] args, File inputDir) {
-//      List<File> refFiles = setupDirTestingAndRefFiles(inputDir);
-//      runCommandLineInterpreter(converterInstance, args);
-//
-//      File[] outFiles = outputDir.listFiles();
-//      Assert.assertTrue("generated files must now exist in: " + getOutputDir(), outFiles != null && outFiles.length > 0);
-//      Assert.assertEquals("generated files must number those in ref", refFiles.size(), outFiles.length);
-//   }
-//
-//   private List<File> setupDirTestingAndRefFiles(File inputDir) {
-//      setupAndCheckInputFiles(inputDir, getInputSuffix());
-//      createAndCleanOutputDir();
-//      List<File> refFiles = createAndCheckRefFiles();
-//      return refFiles;
-//   }
-//
-//   private List<File> setUpFileTestingAndOutput(File inputDir) {
-//      List<File> fileList = setupDirTestingAndRefFiles(inputDir);
-//      return fileList;
-//   }
-//
-//   private void setupAndCheckInputFiles(File inputDir, String suffix) {
-//      List<File> inFiles = getFiles(inputDir, suffix);
-//      Assert.assertTrue("input files (." + suffix + ") must exist in: " + getStartDirName(), inFiles.size() > 0);
-//   }
-//
-//   private List<File> createAndCheckRefFiles() {
-//      List<File> refFiles = getFiles(new File(getReferenceDirName()), getOutputSuffix());
-//      Assert.assertTrue("files must exist in: " + getReferenceDirName(), refFiles.size() > 0);
-//      return refFiles;
-//   }
-//
-//   private void createAndCleanOutputDir() {
-//      outputDir = new File(getOutputDirName());
-//      makeAndCleanDirectory(outputDir);
-//      Assert.assertEquals("clean " + outputDir, 0, getFiles(outputDir).size());
-//   }
-//
-//   private void testFileResults(List<File> refFiles) {
-//      List<File> outFiles = getFiles(outputDir, getOutputSuffix());
-//      Assert.assertTrue("output files must exist in: " + getOutputDir(), outFiles.size() > 0);
-//      Assert.assertEquals("generated files", refFiles.size(), outFiles.size());
-//      for (File refFile : refFiles) {
-//         File outFile = new File(outputDir, refFile.getName());
-//         Assert.assertTrue("file should be created" + outFile, outFile.exists());
-//         compare(refFile, outFile);
-//      }
-//   }
    private void compare(File refFile, File outFile) {
       LOG.debug("comparing " + refFile + " ... " + outFile);
       // try XML
@@ -284,6 +138,7 @@ public class RegressionSuite {
          final File output = outputFileFor(input);
          final File reference = referenceFileFor(input);
          converter.convert(input, output);
+         Assert.assertTrue("file should be created" + output, output.exists());
          compare(reference, output);
       }
    }
