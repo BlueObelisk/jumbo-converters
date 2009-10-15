df968e22b71310e18213a12da731db494733afb5
Jim Downing <jimdowning@pobox.com>
2009-10-15 16:57:34 +0100

    Finished RegressionSuite and used it for the ChemDraw converter

--- jumbo-converters-chemdraw/src/test/java/org/xmlcml/cml/converters/chemdraw/CDX2CDXMLConverterTest.java
+++ jumbo-converters-chemdraw/src/test/java/org/xmlcml/cml/converters/chemdraw/CDX2CDXMLConverterTest.java
@@ -1,55 +1,20 @@
 package org.xmlcml.cml.converters.chemdraw;
 
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
-import java.io.InputStream;
-import nu.xom.Element;
-import nu.xom.ParsingException;
-import nu.xom.ValidityException;
-
-import org.junit.Ignore;
 import org.junit.Test;
-import org.xmlcml.cml.base.CMLBuilder;
-import org.xmlcml.cml.testutil.TestUtils;
+import org.xmlcml.cml.converters.testutils.RegressionSuite;
 
-
-public class CDX2CDXMLConverterTest {
-	
-	public final String getLocalDirName() {
-		return "cdx/cdx";
-	}
-	public final String getInputSuffix() {
-		return "cdx";
-	}
-	public final String getOutputSuffix() {
-		return "cdx.xml";
-	}
-	
+public class CDX2CDXMLConverterTest {	
 	@Test
-	@Ignore // why?
 	public void testConvertToXMLElement() throws IOException {
-		
 		CDX2CDXMLConverter c = new CDX2CDXMLConverter();
-		
-		InputStream in = getClass().getClassLoader().getResourceAsStream("cdx/r19.cdx");
-		File out = File.createTempFile("r19", ".cdx.xml");
-		c.convert(in, out);
-		InputStream expectedFile = getClass().getClassLoader().getResourceAsStream("cdxml/r19Ref.cdx.xml");
-		//Assert.assertTrue("file contents", FileUtils.contentEquals(out, expectedFile));
-		
-		Element cmlRef = null;
-		Element cml = null;
-		try {
-			cmlRef = new CMLBuilder().build(expectedFile).getRootElement();
-			cml = new CMLBuilder().build(new FileInputStream(out)).getRootElement();
-		} catch (ValidityException e) {
-			throw new RuntimeException("BUG: "+e);
-		} catch (ParsingException e) {
-			throw new RuntimeException("BUG: "+e);
-		}
-		TestUtils.assertEqualsCanonically("cml file", cmlRef, cml, true);
+		RegressionSuite r = new RegressionSuite();
+      r.setConverter(c);
+      r.setLocalDirName("cdx/cdx");
+      r.setInputSuffix("cdx");
+      r.setOutputSuffix("cdx.xml");
+      r.run();
 	}
 }
--- jumbo-converters-chemdraw/src/test/java/org/xmlcml/cml/converters/chemdraw/CDXML2CMLConverterTest.java
+++ jumbo-converters-chemdraw/src/test/java/org/xmlcml/cml/converters/chemdraw/CDXML2CMLConverterTest.java
@@ -6,25 +6,13 @@ import org.junit.Test;
 import org.xmlcml.cml.converters.testutils.RegressionSuite;
 
 public class CDXML2CMLConverterTest  {
-	public final String getLocalDirName() {
-		return "cdx/cdxml";
-	}
-	public final String getInputSuffix() {
-		return "cdxml";
-	}
-	public final String getOutputSuffix() {
-		return "cml";
-	}
-	
 	@Test
 	public void testConverter() throws IOException {
       RegressionSuite ce = new RegressionSuite();
       ce.setInputSuffix("cdxml");
       ce.setOutputSuffix("cml");
       ce.setLocalDirName("cdx/cdxml");
-      ce.setConverterInstance(new CDXML2CMLConverter());
+      ce.setConverter(new CDXML2CMLConverter());
       ce.run();
 	}
-
-
 }
