c02f25c64f485a4493558b3db341607130901e20
Jim Downing <jimdowning@pobox.com>
2009-10-16 16:08:35 +0100

    Extended gaussian regression test

--- jumbo-converters-gaussian/src/test/java/org/xmlcml/cml/converters/compchem/gaussian/RegressionTest.java
+++ jumbo-converters-gaussian/src/test/java/org/xmlcml/cml/converters/compchem/gaussian/RegressionTest.java
@@ -1,6 +1,8 @@
 package org.xmlcml.cml.converters.compchem.gaussian;
 
 import org.junit.Test;
+import org.xmlcml.cml.converters.compchem.gaussian.input.CML2GaussianInputConverter;
+import org.xmlcml.cml.converters.compchem.gaussian.log.GaussianLog2CMLConverter;
 import org.xmlcml.cml.converters.testutils.RegressionSuite;
 
 /**
@@ -19,5 +21,17 @@ public class RegressionTest {
    public void gauCML2OWL() {
       RegressionSuite.build("compchem/gaussian/cml2owlrdf", "cml", "rdf",
                             new GaussianCML2OWLRDFConverter()).run();
+   }
+
+   @Test
+   public void gauIn2CML() {
+      RegressionSuite.build("compchem/gaussian/in", "cml", "gau.in",
+                            new CML2GaussianInputConverter());
+   }
+
+   @Test
+   public void gauLog2CML() {
+      RegressionSuite.build("compchem/gaussian/gaussian/log", "g03", "cml",
+                            new GaussianLog2CMLConverter());
    }
 }
