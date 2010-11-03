54d79d0441aa705f7f1720b3d2f56bbce8b2c446
Peter Murray-Rust <pm286@cam.ac.uk>
2010-11-03 15:17:48 +0000

    added mark monroe's gamessus test data

--- jumbo-converters-compchem/src/main/java/org/xmlcml/cml/converters/compchem/gamessus/GamessUSPunchBlock.java
+++ jumbo-converters-compchem/src/main/java/org/xmlcml/cml/converters/compchem/gamessus/GamessUSPunchBlock.java
@@ -68,10 +68,25 @@ public class GamessUSPunchBlock extends AbstractBlock {
 	/*
 	 * keywords in legacy input
 	 */
+	public static final String CISVEC = "CISVEC";
 	public static final String DATA = "DATA";
+	public static final String DIPDR = "DIPDR";
 	public static final String GRAD = "GRAD";
+	public static final String GRAD1 = "GRAD1";
+	public static final String GRAD2 = "GRAD2";
 	public static final String HESSX = "HESS";
+	public static final String IRC = "IRC";
+	public static final String SCF  = "SCF";
+	public static final String SOLEV  = "SOLEV";
+	public static final String SPNORB  = "SPNORB";
+	public static final String SUBCOR  = "SUBCOR";
+	public static final String SUBSCF  = "SUBSCF";
+	public static final String TDIPOLE  = "TDIPOLE";
+	public static final String TVELOCITY  = "TVELOCITY";
+	public static final String TWOEI  = "TWOEI";
 	public static final String VEC  = "VEC";
+	public static final String VEC1  = "VEC1";
+	public static final String VEC2  = "VEC2";
 	public static final String VIB  = "VIB";
 	public static final String ZMAT = "ZMAT";
 
@@ -97,14 +112,45 @@ public class GamessUSPunchBlock extends AbstractBlock {
 	 * 
 	 */
 	public void convertToRawCML() {
-		if (DATA.equals(getBlockName())) {
+		if (false) {
+		} else if (CISVEC.equals(getBlockName())) {
+			makeCisvec();
+		} else if (DATA.equals(getBlockName())) {
 			makeData();
+		} else if (DIPDR.equals(getBlockName())) {
+			makeDipdr();
 		} else if (GRAD.equals(getBlockName())) {
 			makeGrad();
+		} else if (GRAD1.equals(getBlockName())) {
+			makeGrad1();
+		} else if (GRAD2.equals(getBlockName())) {
+			makeGrad2();
 		} else if (HESSX.equals(getBlockName())) {
 			makeHess();
+		} else if (IRC.equals(getBlockName())) {
+			makeIrc();
+		} else if (SCF.equals(getBlockName())) {
+			makeScf();
+		} else if (SOLEV.equals(getBlockName())) {
+			makeSolev();
+		} else if (SPNORB.equals(getBlockName())) {
+			makeSpnorb();
+		} else if (SUBCOR.equals(getBlockName())) {
+			makeSubcor();
+		} else if (SUBSCF.equals(getBlockName())) {
+			makeSubscf();
+		} else if (TDIPOLE.equals(getBlockName())) {
+			makeTdipole();
+		} else if (TVELOCITY.equals(getBlockName())) {
+			makeTvelocity();
+		} else if (TWOEI.equals(getBlockName())) {
+			makeTwoei();
 		} else if (VEC.equals(getBlockName())) {
 			makeVec();
+		} else if (VEC1.equals(getBlockName())) {
+			makeVec1();
+		} else if (VEC2.equals(getBlockName())) {
+			makeVec2();
 		} else if (VIB.equals(getBlockName())) {
 			makeVib();
 		} else if (ZMAT.equals(getBlockName())) {
@@ -134,6 +180,220 @@ public class GamessUSPunchBlock extends AbstractBlock {
 		}
 	}
 
+	
+	private void makeTwoei() {
+		/*
+ $TWOEI
+  9.38230741E+00 3.59976772E-01 4.84824999E+00 2.58194131E+00 3.69772753E-01
+  2.26208416E+00 2.58194146E+00 3.69775829E-01 1.75438331E+00 2.26208429E+00
+...
+  3.50401191E-01 3.56418458E-01 3.21214602E-01 3.50398946E-01 6.05502942E-01
+  2.35039664E-01 2.46416459E-01 2.46416796E-01 6.25819176E-01 6.25819121E-01
+  8.04754778E-01
+  9.38230741E+00 5.33706794E-06 4.84824999E+00 7.31854723E-02 4.94486827E-05
+  2.26208416E+00 7.31854734E-02 4.94572956E-05 1.63533585E-01 2.26208429E+00
+...
+  3.96746625E-03 1.32000268E-03 1.32003348E-03 5.62533556E-02 5.62533408E-02
+  8.04754778E-01
+ $END
+		 */
+		// NYI
+		element = preserveText();
+	}
+
+
+	
+	private void makeCisvec() {
+		/*
+ $CISVEC
+       272 SAPS,   1 STATES
+STATE   1 ENERGY=     -113.7017742428
+  8.829791E-21 -1.869456E-21  3.675079E-21 -3.584421E-08 -7.603602E-21
+  2.703906E-21  2.873688E-21  8.179421E-08 -1.759198E-20  7.454616E-21
+...
+ -3.198973E-05 -1.805055E-05 -5.164041E-04 -1.152809E-19  6.155463E-06
+  4.573113E-06 -2.025012E-06
+ $END
+		 */
+		// NYI
+		element = preserveText();
+	}
+
+	private void makeTdipole() {
+		/**
+		 * 		 $TDIPOLE
+		         1         1        18
+		      -75.0101113553      -75.0101113553
+		        0.0000000000        0.0000000000       -0.6389554616
+		         1         2        18
+		      -75.0101113553      -74.3945819389
+		        0.0000000000        0.0000000000        0.3926137047
+		         2         2        18
+		      -74.3945819389      -74.3945819389
+		        0.0000000000        0.0000000000        0.1297954762
+		 $END
+		 */
+				// NYI
+				element = preserveText();
+			}
+
+	private void makeTvelocity() {
+		/**
+ $TVELOCITY
+         1         2        18
+      -75.0101113553      -74.3945819389
+        0.0000000000        0.0000000000        0.3682047219
+ $END
+		 */
+				// NYI
+				element = preserveText();
+			}
+
+	private void makeIrc() {
+		/**
+ $IRC   PACE=GS2        STRIDE= 0.30000   NPOINT=??
+        STOTAL=  0.299939   NEXTPT=   2
+        NPRT= 0   NPUN= 0   SADDLE=.FALSE.
+        GA(  1)=-5.949328598E-03,-1.396681562E-02, 0.000000000E+00
+        GA(  4)=-8.414775838E-03, 1.241853708E-02, 0.000000000E+00
+        GA(  7)= 1.436410444E-02, 1.548278538E-03, 0.000000000E+00
+ $END   
+*/
+		// NYI
+		element = preserveText();
+	}
+
+	private void makeDipdr() {
+		/**
+		$DIPDR
+		 -2.68829961E+00 0.00000000E+00 0.00000000E+00
+		  0.00000000E+00-1.49612845E-01 0.00000000E+00
+...
+		  0.00000000E+00 7.48204205E-02 7.54407241E-01
+		  0.00000000E+00 1.06457228E+00-2.00610985E-01
+		 $END	 */
+		// NYI
+		element = preserveText();
+	}
+	
+	private void makeSpnorb() {
+		/*
+ $SPNORB ---   CI   SPIN-ORBIT MATRIX ELEMENTS.
+    1 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00
+      0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00
+    1 0.00000000E+00 0.00000000E+00
+    2 0.00000000E+00 0.00000000E+00 1.55948760E-09 0.00000000E+00 0.00000000E+00
+      0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00
+    2 0.00000000E+00 0.00000000E+00
+    3 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 1.52964318E+04
+      0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 6.49682982E+01
+    3 0.00000000E+00 0.00000000E+00
+    4 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00
+      0.00000000E+00-1.52964318E+04 0.00000000E+00 0.00000000E+00 0.00000000E+00
+    4 0.00000000E+00 0.00000000E+00
+    5 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00
+     -6.49682982E+01 0.00000000E+00 0.00000000E+00-1.52964318E+04 0.00000000E+00
+    5 0.00000000E+00 0.00000000E+00
+    6 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00
+      0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00
+    6-1.52964318E+04 0.00000000E+00
+  CI   ADIABATIC STATES---
+          0.000          0.000      15296.432     -15296.432     -15296.432
+     -15296.432
+  CI   SPIN-MIXED STATES---
+     -15296.570     -15296.432     -15296.432          0.000          0.000
+      15296.570
+ $END
+		 */
+		// NYI
+		element = preserveText();
+	}
+	
+	private void makeSolev() {
+		/*
+		 $SOLEV
+		    1  0.0       0.0000    -54.9382263296
+		    2  1.0       0.1380    -54.9382257010
+		    3  1.0       0.1380    -54.9382257010
+		    4  2.0   15296.5697    -54.8685312164
+		    5  2.0   15296.5697    -54.8685312164
+		    6  0.0   30593.1395    -54.7988361032
+		 $END
+		 */
+		// NYI
+		element = preserveText();
+	}
+	
+	private void makeScf() {
+		//  $SCF    CICOEF( 1)=  0.97750533, -0.21091073 $END   
+		// NYI
+		element = preserveText();
+	}
+	
+	private void makeSubscf() {
+/**
+ $SUBSCF
+! SUBSYSTEM     1
+  1  7  0
+  2  8  3  9  0
+  0
+! SUBSYSTEM     2
+   2   8   0
+   1   7   3   9   4  10   0
+   0
+! SUBSYSTEM     3
+   3   9   0
+   1   7   2   8   4  10   5  11   0
+   0
+! SUBSYSTEM     4
+   4  10   0
+   2   8   3   9   5  11   6  12   0
+   0
+! SUBSYSTEM     5
+   5  11   0
+   3   9   4  10   6  12   0
+   0
+! SUBSYSTEM     6
+   6  12   0
+   4  10   5  11   0
+   0
+ $END
+ */
+		// NYI
+		element = preserveText();
+	}
+	
+	private void makeSubcor() {
+/**
+ $SUBCOR
+! SUBSYSTEM     1
+  1  7  0
+  2  8  0
+  0
+! SUBSYSTEM     2
+  2  8  0
+  1  7  3  9  0
+  0
+! SUBSYSTEM     3
+   3   9   0
+   2   8   4  10   0
+   0
+! SUBSYSTEM     4
+   4  10   0
+   3   9   5  11   0
+   0
+! SUBSYSTEM     5
+   5  11   0
+   4  10   6  12   0
+   0
+! SUBSYSTEM     6
+   6  12   0
+   5  11   0
+   0
+ $END
+ */
+	}
+	
 	private void makeAnonymous() {
 		lineCount = 0;
 		CMLModule module = new CMLModule();
@@ -197,6 +457,40 @@ E=      -37.2380397698, E(NUC)=    5.9560361192
  .. 7 lines overall
  7  1 0.00000000E+00 0.00000000E+00 1.12514651E+00 0.00000000E+00 0.00000000E+00
  7  2 8.36936903E-01-8.36936903E-01
+		 */
+		element = new CMLModule();
+		element.appendChild(makePackedLines());
+	}
+
+	private void makeVec1() {
+		/**
+ $VEC1  
+ 1  1 1.00173154E+00-4.42355961E-04-8.94950868E-03 0.00000000E+00 0.00000000E+00
+ 1  2 1.48836591E-04 0.00000000E+00 0.00000000E+00 8.83998840E-04-3.30678346E-04
+ 1  3 5.47496681E-04-2.16818336E-04 0.00000000E+00 0.00000000E+00 0.00000000E+00
+ 1  4-2.39910669E-03 1.14463450E-03 0.00000000E+00-5.27016825E-04-4.94322215E-04
+ 1  5-2.39910669E-03 1.14463450E-03 0.00000000E+00 5.27016825E-04-4.94322215E-04
+ 2  1-6.40850833E-03 3.70026464E-01 2.83584610E-01 0.00000000E+00 0.00000000E+00
+...
+ 7  4 6.44523230E-01 2.20422384E-01 0.00000000E+00-1.46637220E-02-6.72413304E-03
+ 7  5-6.44523230E-01-2.20422384E-01 0.00000000E+00-1.46637220E-02 6.72413304E-03
+		 */
+		element = new CMLModule();
+		element.appendChild(makePackedLines());
+	}
+
+	private void makeVec2() {
+		/**
+ $VEC2
+ 1  1 1.00173154E+00-4.42355961E-04-8.94950868E-03 0.00000000E+00 0.00000000E+00
+ 1  2 1.48836591E-04 0.00000000E+00 0.00000000E+00 8.83998840E-04-3.30678346E-04
+ 1  3 5.47496681E-04-2.16818336E-04 0.00000000E+00 0.00000000E+00 0.00000000E+00
+ 1  4-2.39910669E-03 1.14463450E-03 0.00000000E+00-5.27016825E-04-4.94322215E-04
+ 1  5-2.39910669E-03 1.14463450E-03 0.00000000E+00 5.27016825E-04-4.94322215E-04
+ 2  1-6.40850833E-03 3.70026464E-01 2.83584610E-01 0.00000000E+00 0.00000000E+00
+...
+ 7  4 6.44523230E-01 2.20422384E-01 0.00000000E+00-1.46637220E-02-6.72413304E-03
+ 7  5-6.44523230E-01-2.20422384E-01 0.00000000E+00-1.46637220E-02 6.72413304E-03
 		 */
 		element = new CMLModule();
 		element.appendChild(makePackedLines());
@@ -322,6 +616,52 @@ N3           7.   -1.5142322939E-02    4.8724521568E-03   -3.9199279015E-03
 		element = module;
 	}
 
+	private void makeGrad1() {
+		CMLModule module = new CMLModule();
+		module.setRole(abstractCommon.getPrefix());
+		/*
+E=     -417.0071979209  GMAX=    .0518798  GRMS=    .0144469
+H1           1.    1.4236802098E-02   -1.2652467333E-02   -2.0583432080E-03
+C2           6.   -1.7931214639E-03   -2.3258102960E-02    2.7744336442E-03
+N3           7.   -1.5142322939E-02    4.8724521568E-03   -3.9199279015E-03
+		 */
+		lineCount = 0;
+		
+		String flags = lines.get(lineCount++);
+		JumboFormat jumboFormat = new JumboFormat();
+		List<CMLScalar> scalars = jumboFormat.addParsedScalars(
+			module,
+			abstractCommon.getPrefix(), 
+			"('E='F20.10'  GMAX='F12.7'  GRMS='F12.7)", 
+			flags, 
+			new String[]{F_+E, F_+GMAX, F_+GRMS});
+		addMoleculeAsColumns(module, "A10F5.1F20.10F20.10F20.10");
+		element = module;
+	}
+
+	private void makeGrad2() {
+		CMLModule module = new CMLModule();
+		module.setRole(abstractCommon.getPrefix());
+		/*
+E=     -417.0071979209  GMAX=    .0518798  GRMS=    .0144469
+H1           1.    1.4236802098E-02   -1.2652467333E-02   -2.0583432080E-03
+C2           6.   -1.7931214639E-03   -2.3258102960E-02    2.7744336442E-03
+N3           7.   -1.5142322939E-02    4.8724521568E-03   -3.9199279015E-03
+		 */
+		lineCount = 0;
+		
+		String flags = lines.get(lineCount++);
+		JumboFormat jumboFormat = new JumboFormat();
+		List<CMLScalar> scalars = jumboFormat.addParsedScalars(
+			module,
+			abstractCommon.getPrefix(), 
+			"('E='F20.10'  GMAX='F12.7'  GRMS='F12.7)", 
+			flags, 
+			new String[]{F_+E, F_+GMAX, F_+GRMS});
+		addMoleculeAsColumns(module, "A10F5.1F20.10F20.10F20.10");
+		element = module;
+	}
+
 	private final static Pattern hessPattern = Pattern.compile(
 			"ENERGY IS(....................) E\\(NUC\\) IS(....................).*");
 
@@ -402,7 +742,6 @@ N3           7.   -1.5142322939E-02    4.8724521568E-03   -3.9199279015E-03
 	private void makeZmat() {
 		/**
  $ZMAT   IZMAT(1)=
- // I have NO idea what these lines are. I hope there are exactly two
          3,   2,   3,   5,   7,
          3,   3,   5,   7,  13,
          1,   1,   2,
@@ -502,20 +841,30 @@ N3           7.   -1.5142322939E-02    4.8724521568E-03   -3.9199279015E-03
 --- CLOSED SHELL ORBITALS --- GENERATED AT Mon Oct 25 13:07:35 2010
 Methylene...1-A-1 state...RHF/STO-2G                                            
 E(RHF)=      -37.2322678015, E(NUC)=    6.1221376700,    8 ITERS
+or 
+E(ROHF)=      -37.2778767090, E(NUC)=    6.1450367257,    7 ITERS
 		 */
 		lineCount++;
+		Pattern pattern = Pattern.compile("E\\(([^\\)]+)\\)=\\s*([\\-\\.\\d]+), E\\(([^\\)]+)\\)=\\s*([\\-\\.\\d]+),\\s+(\\d+) ITERS");
+//		Pattern pattern = Pattern.compile("E\\(([^\\)]+)\\)=\\s*([\\-\\.\\d]+), E\\(([^\\)]+)\\)=\\s*([\\-\\.\\d]+),\\s+.*");
+		System.out.println(pattern);
 		CMLScalar title = new CMLScalar(lines.get(lineCount++));
 		module.appendChild(title);
-		new JumboFormat().addParsedScalars(module, abstractCommon.getPrefix(),  
-				"'E(RHF)='F20.10', E(NUC)='F16.10','I5' ITERS'", 
-				lines.get(lineCount++), new String[]{F_+ERHF, F_+ENUC, I_+ITERS});
+		System.out.println(lines.get(lineCount));
+		new JumboFormat().addParsedScalars(module, abstractCommon.getPrefix(),  pattern, 
+		lines.get(lineCount++), new String[]{A_+DICTREF, F_+ERHF, A_+DICTREF, F_+ENUC, I_+ITERS});
 	}
 
-	private void preserveText(CMLModule module) {
+	private CMLModule preserveText(CMLModule module) {
 		String line = Util.concatenate((String[])lines.toArray(new String[0]), CMLConstants.S_NEWLINE);
 		CMLScalar scalar = new CMLScalar(line);
 		module.appendChild(scalar);
 		this.setBlockName(UNKNOWN);
+		return module;
+	}
+	
+	private CMLModule preserveText() {
+		return preserveText(new CMLModule());
 	}
 
 	private void nserch(CMLModule module) {
