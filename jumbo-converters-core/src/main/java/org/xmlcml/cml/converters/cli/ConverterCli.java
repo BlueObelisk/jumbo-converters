15326baadefe62767b5d71e87e8279cf5383bbf8
Peter Murray-Rust <pm286@cam.ac.uk>
2011-02-09 15:44:46 +0000

    added debug to converterCLI

--- jumbo-converters-core/src/main/java/org/xmlcml/cml/converters/cli/ConverterCli.java
+++ jumbo-converters-core/src/main/java/org/xmlcml/cml/converters/cli/ConverterCli.java
@@ -1,8 +1,10 @@
 package org.xmlcml.cml.converters.cli;
 
+import org.apache.log4j.Logger;
 import org.xmlcml.cml.converters.Converter;
 import org.xmlcml.cml.converters.registry.ConverterInfo;
 import org.xmlcml.cml.converters.registry.ConverterRegistry;
+import org.xmlcml.euclid.Util;
 
 import java.io.File;
 
@@ -11,16 +13,12 @@ import java.io.File;
  */
 public class ConverterCli {
 
+	private static Logger LOG = Logger.getLogger(Converter.class);
+	
     public static void main(String[] args) {
 
         if (args.length != 6) {
-            System.out.println("Usage:");
-            System.out.println("   -i gau-arc foo.arc -o cml foo.cml");
-            System.out.println();
-            System.out.println("Available converters:");
-            for (ConverterInfo info : ConverterRegistry.getConverterList()) {
-                System.out.println(info.getInType()+"\t"+info.getOutType()+"\t"+info.getName());
-            }
+            usage();
         } else {
 
             String intype = args[1];
@@ -28,13 +26,29 @@ public class ConverterCli {
             String outtype = args[4];
             String outfilename = args[5];
 
+            Converter converter = ConverterRegistry.findConverter(intype, outtype);
+            LOG.info("Converter: "+converter);
+            if (converter == null) {
+            	usage();
+            	throw new RuntimeException("cannot find converter for: "+Util.concatenate(args, " "));
+            }
+            
             File infile = new File(infilename);
             File outfile = new File(outfilename);
 
-            Converter converter = ConverterRegistry.findConverter(intype, outtype);
             converter.convert(infile, outfile);
 
         }
     }
+
+	private static void usage() {
+		System.out.println("Usage:");
+		System.out.println("   -i gau-arc foo.arc -o cml foo.cml");
+		System.out.println();
+		System.out.println("Available converters:");
+		for (ConverterInfo info : ConverterRegistry.getConverterList()) {
+		    System.out.println(info.getInType()+"\t"+info.getOutType()+"\t"+info.getName());
+		}
+	}
 
 }
