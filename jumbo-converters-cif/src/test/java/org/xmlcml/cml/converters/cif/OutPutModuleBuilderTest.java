262bcaf4da42411b93f2180387a54c13a441525d
nickengland <nickengland@localhost>
2011-02-15 19:10:05 +0000

    Changed CIF parser to set the appropriate dataType from the CIF dictionary when parsing a rawCMl2CMl file.

--- jumbo-converters-cif/src/test/java/org/xmlcml/cml/converters/cif/OutPutModuleBuilderTest.java
+++ jumbo-converters-cif/src/test/java/org/xmlcml/cml/converters/cif/OutPutModuleBuilderTest.java
@@ -1,10 +1,17 @@
 package org.xmlcml.cml.converters.cif;
 
+import javax.print.attribute.standard.OutputDeviceAssigned;
+
 import nu.xom.Element;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.xmlcml.cml.converters.cif.dict.CifDictionaryBuilder;
+import org.xmlcml.cml.converters.cif.dict.UnitsDictionary;
+import org.xmlcml.cml.element.CMLEntry;
 import org.xmlcml.cml.element.CMLMolecule;
+import org.xmlcml.cml.element.CMLProperty;
+import org.xmlcml.cml.element.CMLScalar;
 
 public class OutPutModuleBuilderTest {
 
@@ -20,8 +27,8 @@ public class OutPutModuleBuilderTest {
         Assert.assertEquals(false, builder.isFinalised);
         builder.finalise();
         Assert.assertEquals(true, builder.isFinalised);
-        Assert.assertTrue(builder.moleculeModule.getParent()==builder.crystalModule);
-        Assert.assertTrue(builder.crystalModule.getParent()==builder.topModule);
+        Assert.assertTrue(builder.moleculeModule.getParent() == builder.crystalModule);
+        Assert.assertTrue(builder.crystalModule.getParent() == builder.topModule);
     }
 
     @Test
@@ -29,7 +36,7 @@ public class OutPutModuleBuilderTest {
         OutPutModuleBuilder builder = new OutPutModuleBuilder();
         CMLMolecule mol = new CMLMolecule();
         builder.addToTop(mol);
-        Assert.assertTrue(mol.getParent()==builder.topModule);
+        Assert.assertTrue(mol.getParent() == builder.topModule);
     }
 
     @Test
@@ -37,7 +44,7 @@ public class OutPutModuleBuilderTest {
         OutPutModuleBuilder builder = new OutPutModuleBuilder();
         CMLMolecule mol = new CMLMolecule();
         builder.addToCrystal(mol);
-        Assert.assertTrue(mol.getParent()==builder.crystalModule);
+        Assert.assertTrue(mol.getParent() == builder.crystalModule);
     }
 
     @Test
@@ -45,24 +52,52 @@ public class OutPutModuleBuilderTest {
         OutPutModuleBuilder builder = new OutPutModuleBuilder();
         CMLMolecule mol = new CMLMolecule();
         builder.addToMolecule(mol);
-        Assert.assertTrue(mol.getParent()==builder.moleculeModule);
+        Assert.assertTrue(mol.getParent() == builder.moleculeModule);
     }
-    @Test(expected=IllegalArgumentException.class)
-    public void nullElement(){
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullElement() {
         OutPutModuleBuilder builder = new OutPutModuleBuilder();
         CMLMolecule mol = null;
         builder.addToMolecule(mol);
     }
+
     @Test
-    public void testAddAllChildren(){
+    public void testAddAllChildren() {
         OutPutModuleBuilder builder = new OutPutModuleBuilder();
-        Element root= new Element("root");
+        Element root = new Element("root");
         root.appendChild(new Element("child1"));
         root.appendChild(new Element("child2"));
-        root.appendChild("text"); //Text nodes not copied
+        root.appendChild("text"); // Text nodes not copied
         Assert.assertEquals(3, root.getChildCount());
         builder.addAllChildrenToTop(root);
         Assert.assertEquals(2, builder.topModule.getChildCount());
     }
-    
+
+    @Test
+    public void testIndexDictionary() {
+        OutPutModuleBuilder builder = new OutPutModuleBuilder();
+        int sizeOfMap = builder.idMap.keySet().size();
+        System.out.println(sizeOfMap);
+        // CMLEntry entry=builder.idMap.get("cell_measurement_theta_max");
+        CMLEntry entry = builder.idMap.get("cell_measurement_wavelength");
+        for (CMLEntry test : builder.idMap.values()) {
+            System.out.println(test.getId());
+        }
+        Assert.assertNotNull(entry);
+    }
+
+    @Test
+    public void testFetchDataType() {
+        OutPutModuleBuilder builder = new OutPutModuleBuilder();
+        CMLProperty prop = new CMLProperty();
+        prop.addNamespaceDeclaration(CifDictionaryBuilder.PREFIX, CifDictionaryBuilder.URI);
+        prop.setDictRef("iucr:cell_measurement_wavelength");
+        prop.addScalar(new CMLScalar("0.0622"));
+        String type = prop.getScalarElements().get(0).getDataType();
+        Assert.assertEquals("xsd:string", type);
+        builder.fetchDataType(prop);
+        type = prop.getScalarElements().get(0).getDataType();
+        Assert.assertEquals("xsd:double", type);
+    }
 }
