67624a17c46f25f7c549c3b54348ab7967941ff1
nickengland <nickengland@localhost>
2011-02-23 14:17:39 +0000

    added parsing of errors in doubles from CIFs

--- jumbo-converters-cif/src/main/java/org/xmlcml/cml/converters/cif/OutPutModuleBuilder.java
+++ jumbo-converters-cif/src/main/java/org/xmlcml/cml/converters/cif/OutPutModuleBuilder.java
@@ -9,11 +9,14 @@ import nu.xom.Attribute;
 import nu.xom.Document;
 import nu.xom.Element;
 import nu.xom.Elements;
+import nu.xom.Nodes;
 import nu.xom.ParsingException;
 import nu.xom.ValidityException;
 
 import org.xmlcml.cml.base.CMLBuilder;
+import org.xmlcml.cml.base.CMLConstants;
 import org.xmlcml.cml.base.CMLElement;
+import org.xmlcml.cml.converters.cif.dict.CifDictionaryBuilder;
 import org.xmlcml.cml.element.CMLCml;
 import org.xmlcml.cml.element.CMLDictionary;
 import org.xmlcml.cml.element.CMLEntry;
@@ -21,24 +24,23 @@ import org.xmlcml.cml.element.CMLModule;
 import org.xmlcml.cml.element.CMLScalar;
 
 public class OutPutModuleBuilder {
-    public static final String CONVETION_URI="http://www.xml-cml.org/convention/";
-    public static final String CONVETION_PREFIX="convention";
+    public static final String CONVETION_URI = "http://www.xml-cml.org/convention/";
+    public static final String CONVETION_PREFIX = "convention";
     public static final String CONVENTION_CRYSTALOGRAPHY = "CrystalographyExperiment";
     public static final String CONVENTION_CRYSTAL = "crystalStructure";
     public static final String CONVENTION_MOLECULAR = "molecular";
-    public static final String IUCR_DICT_URI = "http://www.iucr.org/cif";
-    public static final String IUCR_DICT_PREFIX = "iucr";
+    public static final String IUCR_DICT_URI = CifDictionaryBuilder.URI;
+    public static final String IUCR_DICT_PREFIX = CifDictionaryBuilder.PREFIX;
     public static final String XHTML_PREFIX = "xhtml";
     public static final String XHTML_URI = "http://www.w3.org/1999/xhtml";
-    private String _dict_path="src/main/resources/cif-dictionary.cml";
-    
+    private String _dict_path = "src/main/resources/cif-dictionary.cml";
     CMLDictionary dict;
     Map<String, CMLEntry> idMap;
     CMLModule topModule;
     CMLModule crystalModule;
     CMLModule moleculeModule;
     CMLCml cml;
-    
+
     public CMLModule getTopModule() {
         return topModule;
     }
@@ -58,38 +60,41 @@ public class OutPutModuleBuilder {
     boolean isFinalised = false;
 
     /**
-     * Creates an OutputModuleBuilder with a non-default path to a dictionary for dataType resolution
-     * @param path to CML dictionary
+     * Creates an OutputModuleBuilder with a non-default path to a dictionary
+     * for dataType resolution
+     * 
+     * @param path
+     *            to CML dictionary
      */
-    public OutPutModuleBuilder(String dictPath){
-        this._dict_path=dictPath;
+    public OutPutModuleBuilder(String dictPath) {
+        this._dict_path = dictPath;
         init();
     }
-    
+
     public OutPutModuleBuilder() {
         init();
     }
 
-    private  void loadDict(String path) throws ValidityException, ParsingException, IOException{
+    private void loadDict(String path) throws ValidityException, ParsingException, IOException {
         CMLBuilder builder = new CMLBuilder();
-        Document doc=builder.build(new File(path));
-        CMLDictionary dict=(CMLDictionary)doc.getRootElement();
-        this.dict=dict;
-        idMap=new HashMap<String, CMLEntry>();
-        for(CMLEntry entry:dict.getEntryElements()){
+        Document doc = builder.build(new File(path));
+        CMLDictionary dict = (CMLDictionary) doc.getRootElement();
+        this.dict = dict;
+        idMap = new HashMap<String, CMLEntry>();
+        for (CMLEntry entry : dict.getEntryElements()) {
             this.idMap.put(entry.getId(), entry);
         }
     }
-    
+
     private void init() {
         cml = new CMLCml();
         cml.addNamespaceDeclaration("convention", CONVETION_URI);
         topModule = new CMLModule();
-        topModule.addAttribute(new Attribute("convention", CONVETION_PREFIX+":"+CONVENTION_CRYSTALOGRAPHY));
+        topModule.addAttribute(new Attribute("convention", CONVETION_PREFIX + ":" + CONVENTION_CRYSTALOGRAPHY));
         crystalModule = new CMLModule();
-        crystalModule.addAttribute(new Attribute("convention", CONVETION_PREFIX+":"+CONVENTION_CRYSTAL));
+        crystalModule.addAttribute(new Attribute("convention", CONVETION_PREFIX + ":" + CONVENTION_CRYSTAL));
         moleculeModule = new CMLModule();
-        moleculeModule.addAttribute(new Attribute("convention", CONVETION_PREFIX+":"+CONVENTION_MOLECULAR));
+        moleculeModule.addAttribute(new Attribute("convention", CONVETION_PREFIX + ":" + CONVENTION_MOLECULAR));
 
         cml.addNamespaceDeclaration(IUCR_DICT_PREFIX, IUCR_DICT_URI);
         cml.addNamespaceDeclaration(XHTML_PREFIX, XHTML_URI);
@@ -99,15 +104,15 @@ public class OutPutModuleBuilder {
         } catch (ValidityException e) {
             System.err.println("Dictionary not valid");
             e.printStackTrace();
-            this.dict=null;
+            this.dict = null;
         } catch (ParsingException e) {
             System.err.println("Cannot parse dictionary");
             e.printStackTrace();
-            this.dict=null;
+            this.dict = null;
         } catch (IOException e) {
             System.err.println("IOException reading dictionary");
             e.printStackTrace();
-            this.dict=null;
+            this.dict = null;
         }
     }
 
@@ -115,50 +120,89 @@ public class OutPutModuleBuilder {
         if (!isFinalised) {
             topModule.appendChild(crystalModule);
             crystalModule.appendChild(moleculeModule);
+            Nodes nodes = this.cml.query("//cml:*[@dictRef]", CMLConstants.CML_XPATH);
+            for (int x = 0; x < nodes.size(); x++) {
+                CMLElement elem = (CMLElement) nodes.get(x);
+                fetchDataType(elem);
+            }
         }
         isFinalised = true;
     }
 
-    protected void fetchDataType(CMLElement element){
+    protected void fetchDataType(CMLElement element) {
         Attribute dictURI = element.getAttribute("dictRef");
-        String dictRef=dictURI.getValue();
-        int pos=dictRef.indexOf(':');
-        if(pos==-1){
-            System.err.println("Cannot dereference non-namespaced reference: "+dictRef);
+        // If no dictionary, don't do anything
+        if (dictURI == null) {
             return;
         }
-        String prefix=dictRef.substring(0, pos);
-        String id=dictRef.substring(pos+1);
-        String URI=element.getNamespaceURI(prefix);
-        if(!dict.getNamespace().equals(URI)){
-            System.err.println("Dictionary URI: "+dict.getNamespace()+" does not match "+URI+ " from file");
+        String dictRef = dictURI.getValue();
+        int pos = dictRef.indexOf(':');
+        if (pos == -1) {
+            System.err.println("Cannot dereference non-namespaced reference: " + dictRef);
             return;
         }
-        CMLEntry dictEntry=idMap.get(id);
-        if(dictEntry==null){
-            System.err.println("No entry matches "+id);
+        // String prefix = dictRef.substring(0, pos);
+        String id = dictRef.substring(pos + 1);
+        String standardURI = CifDictionaryBuilder.URI;
+        if (!dict.getNamespace().equals(standardURI)) {
+            System.err.println("Dictionary URI: " + dict.getNamespace() + " does not match " + standardURI + " from file");
             return;
         }
-        Elements elems =element.getChildCMLElements("scalar");
-        for(int x=0;x<elems.size();x++){
-            CMLScalar scalar = (CMLScalar)elems.get(x);
-            String type=dictEntry.getDataType();
-            if("xsd:float".equals(type)||"xsd:double".equals(type)){
-                double d=Double.parseDouble(scalar.getValue());
+        CMLEntry dictEntry = idMap.get(id);
+        if (dictEntry == null) {
+            System.err.println("No entry matches " + id);
+            return;
+        }
+        Elements elems = element.getChildCMLElements("scalar");
+        for (int x = 0; x < elems.size(); x++) {
+            CMLScalar scalar = (CMLScalar) elems.get(x);
+            String type = dictEntry.getDataType();
+            if ("xsd:float".equals(type) || "xsd:double".equals(type)) {
+                double d = getDoubleFromString(scalar.getValue());
+                double e = getErrorFromString(scalar.getValue());
                 scalar.setValue(d);
-            }
-            else if("xsd:string".equals(type)){
-                String s=scalar.getValue();
+                scalar.setErrorValue(e);
+            } else if ("xsd:string".equals(type)) {
+                String s = scalar.getValue();
                 scalar.setValue(s);
             }
         }
     }
-    
+
+    protected double getErrorFromString(String value) {
+        int x = value.indexOf('(');
+        int y = value.indexOf('.');
+        if(x==-1){
+            return 0;
+        }
+        int error = Integer.parseInt(value.substring(x+1,value.indexOf(')')));
+        
+        int exponent;
+        if(y==-1){
+            exponent=0;
+        }
+        else{
+            exponent=y-(x-1);
+        }
+        double d= error*Math.pow(10, exponent);
+        return d;
+    }
+
+    protected double getDoubleFromString(String value) {
+        int x = value.indexOf('(');
+        String number = value.substring(0, (x == -1) ? value.length() : x);
+        double d = Double.parseDouble(number);
+        return d;
+    }
+
     public void addToTop(Element element) {
         if (element == null) {
             throw new IllegalArgumentException("Cannot add null element");
         }
         element.detach();
+        if (element instanceof CMLElement) {
+            fetchDataType((CMLElement) element);
+        }
         this.topModule.appendChild(element);
     }
 
@@ -167,6 +211,9 @@ public class OutPutModuleBuilder {
             throw new IllegalArgumentException("Cannot add null element");
         }
         element.detach();
+        if (element instanceof CMLElement) {
+            fetchDataType((CMLElement) element);
+        }
         this.crystalModule.appendChild(element);
     }
 
@@ -175,6 +222,9 @@ public class OutPutModuleBuilder {
             throw new IllegalArgumentException("Cannot add null element");
         }
         element.detach();
+        if (element instanceof CMLElement) {
+            fetchDataType((CMLElement) element);
+        }
         this.moleculeModule.appendChild(element);
     }
 
@@ -197,8 +247,8 @@ public class OutPutModuleBuilder {
         if (id != null) {
             this.topModule.setId(id);
         }
-        String title= parent.getTitle();
-        if(title!=null){
+        String title = parent.getTitle();
+        if (title != null) {
             this.topModule.setTitle(title);
         }
     }
