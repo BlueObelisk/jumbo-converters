3b0463a4c41e94ff1540e4ef4c7f6cf4537019c6
Peter Murray-Rust <733515+petermr@users.noreply.github.com>
2011-03-01 15:13:53 +0000

    first gaussian hierarchy demo

--- jumbo-converters-compchem/src/test/java/org/xmlcml/cml/converters/compchem/gaussian/log/GaussianLog2XMLConverterTest.java
+++ jumbo-converters-compchem/src/test/java/org/xmlcml/cml/converters/compchem/gaussian/log/GaussianLog2XMLConverterTest.java
@@ -22,4 +22,28 @@ public class GaussianLog2XMLConverterTest {
         File out = new File(new File("."), "target/test/compchem/gaussian/test1.xml");
 		glc.convert(inputStream, out);
 	}
+	
+	@Test
+	public void testFrameMini() throws Exception {
+		InputStream templateStream = Util.getInputStreamFromResource(
+				"org/xmlcml/cml/converters/compchem/gaussian/log/templateListFrame.xml");
+		Element templateXML = new Builder().build(templateStream).getRootElement();
+		InputStream inputStream = Util.getInputStreamFromResource(
+		        "compchem/gaussian/log/examples/hashpmini.log");
+		GaussianLog2XMLConverter glc = new GaussianLog2XMLConverter(templateXML);
+        File out = new File(new File("."), "target/test/compchem/gaussian/hashpmini.xml");
+		glc.convert(inputStream, out);
+	}
+	
+	@Test
+	public void testFrame() throws Exception {
+		InputStream templateStream = Util.getInputStreamFromResource(
+				"org/xmlcml/cml/converters/compchem/gaussian/log/templateListFrame.xml");
+		Element templateXML = new Builder().build(templateStream).getRootElement();
+		InputStream inputStream = Util.getInputStreamFromResource(
+		        "compchem/gaussian/log/examples/hashp.log");
+		GaussianLog2XMLConverter glc = new GaussianLog2XMLConverter(templateXML);
+        File out = new File(new File("."), "target/test/compchem/gaussian/hashp.xml");
+		glc.convert(inputStream, out);
+	}
 }
