9f937b43db9e6e76d7c81ba9033147f96b197f41
Peter Murray-Rust <pm286@cam.ac.uk>
2011-03-02 01:04:13 +0000

    added toplevel link entities

--- jumbo-converters-compchem/src/test/java/org/xmlcml/cml/converters/compchem/gaussian/log/ConverterTest.java
+++ jumbo-converters-compchem/src/test/java/org/xmlcml/cml/converters/compchem/gaussian/log/ConverterTest.java
@@ -1,12 +1,10 @@
 package org.xmlcml.cml.converters.compchem.gaussian.log;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 import org.junit.Ignore;
 import org.junit.Test;
 import org.xmlcml.cml.converters.compchem.TestUtils;
-import org.xmlcml.cml.converters.compchem.gaussian.log.old.GaussianLog2XMLConverterOld;
 import org.xmlcml.cml.converters.testutils.RegressionSuite;
 import org.xmlcml.euclid.Util;
 
