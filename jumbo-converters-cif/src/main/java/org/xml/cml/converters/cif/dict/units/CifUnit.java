1e239b176bdc24883988d97d1fa5a7d2c5b286ce
nickengland <nickengland@localhost>
2011-03-04 12:01:38 +0000

    added all the units from the CIF dictionary

--- jumbo-converters-cif/src/main/java/org/xml/cml/converters/cif/dict/units/CifUnit.java
+++ jumbo-converters-cif/src/main/java/org/xml/cml/converters/cif/dict/units/CifUnit.java
@@ -1,15 +1,35 @@
 package org.xml.cml.converters.cif.dict.units;
 
-import org.xmlcml.cml.base.CMLConstants;
 import org.xmlcml.cml.converters.cif.dict.UnitsDictionary;
 
 public enum CifUnit {
-    deg_min("test"),min("test2");
+    min(""),
+    deg_min(""),
+    mm(""),
+    kW(""),
+    A3(""),
+    kV(""),
+    A(""),
+    A_MINUS_1("A-1",""),
+    Da(""),
+    mm_MINUS_1("mm-1",""),
+    kPa(""),
+    A2(""),
+    degrees(""),
+    dimensionless(""),
+    K(""),
+    fm(""),
+    sec(""),
+    deg(""),
+    mA(""),
+    Mg_MINUS_3("Mg-3",""),
+    e_A_MINUS_3("e_A-3",""),
+    Mgm_MINUS_3("Mgm-3","");
     
     
     String cmlUnitId;
-    String cmlPrefixString="cml";
-    String cmlNamespace=CMLConstants.CML_NS;
+    String cmlUnitPrefixString="allUnits";
+    String cmlUnitNamespace="http://www.xml-cml.org/units/allUnits/";
     
     String cifUnitId;
     String cifPrefix=UnitsDictionary.PREFIX;
@@ -19,5 +39,10 @@ public enum CifUnit {
         this.cifUnitId=this.name();
         this.cmlUnitId=cmlUnitId;
     }
+    CifUnit (String cifID, String cmlUnitId){
+    	this.cifUnitId=cifID;
+    	this.cmlUnitId=cmlUnitId;
+    }
+    
     
 }
