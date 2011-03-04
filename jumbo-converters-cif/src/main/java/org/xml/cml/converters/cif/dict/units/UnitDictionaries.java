c8870facc59b42eddc810679797298b90e3f331e
nickengland <nickengland@gmail.com>
2011-03-04 16:11:54 +0000

    Added unit dictionaries to map between multiple different dictionaries and CIF

---
+++ jumbo-converters-cif/src/main/java/org/xml/cml/converters/cif/dict/units/UnitDictionaries.java
@@ -0,0 +1,15 @@
+package org.xml.cml.converters.cif.dict.units;
+
+public enum UnitDictionaries {
+    allUnits("allUnits","http://www.xml-cml.org/units/allUnits/"),
+    si("si","http://xml-cml.org/unit/si");
+    
+    public String prefix;
+    public String URI;
+    
+    private UnitDictionaries(String prefix, String URI) {
+       this.prefix=prefix;
+       this.URI=URI;
+    }
+
+}
