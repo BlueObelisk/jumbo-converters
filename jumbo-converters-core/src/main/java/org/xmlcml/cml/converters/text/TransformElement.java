88040a4bd382d3868e1b85211ea46b7c92af955f
Peter Murray-Rust <733515+petermr@users.noreply.github.com>
2011-03-19 09:19:16 +0000

    alpha version of grouping for NWChem

--- jumbo-converters-core/src/main/java/org/xmlcml/cml/converters/text/TransformElement.java
+++ jumbo-converters-core/src/main/java/org/xmlcml/cml/converters/text/TransformElement.java
@@ -5,6 +5,7 @@ import nu.xom.Element;
 import nu.xom.Elements;
 import nu.xom.Node;
 import nu.xom.Nodes;
+import nu.xom.ParentNode;
 
 import org.apache.log4j.Logger;
 import org.xmlcml.cml.attribute.DictRefAttribute;
@@ -12,6 +13,8 @@ import org.xmlcml.cml.base.CMLConstants;
 import org.xmlcml.cml.base.CMLUtil;
 import org.xmlcml.cml.converters.Outputter;
 import org.xmlcml.cml.converters.Outputter.OutputLevel;
+import org.xmlcml.cml.element.CMLMatrix;
+import org.xmlcml.cml.element.CMLModule;
 import org.xmlcml.cml.element.CMLScalar;
 import org.xmlcml.cml.element.CMLVector3;
 import org.xmlcml.cml.interfacex.HasDictRef;
@@ -30,12 +33,16 @@ public class TransformElement implements MarkupApplier {
 	private static final String VALUE = "value";
 
 	// process values
+	private static final String ADDROLE = "addRole";
 	private static final String DATATYPE = "dataType";
+	private static final String GROUP = "group";
+	private static final String MATRIX33 = "matrix33";
 	private static final String NAMEVALUE = "nameValue";
 	private static final String PULLUP_SINGLETON = "pullupSingleton";
 	private static final String RENAME = "rename";
 	private static final String TEMPLATE_REF = "templateRef";
 	private static final String VECTOR3 = "vector3";
+
 	
 	private Element element;
 	private OutputLevel outputLevel;
@@ -106,20 +113,67 @@ public class TransformElement implements MarkupApplier {
 	public void applyMarkup(LineContainer lineContainer) {
 		
 		parsedElement = lineContainer.getLinesElement();
-		if (NAMEVALUE.equals(process)) {
-			createNameValue();
-		} else if (RENAME.equals(process)) {
-			rename();
+		if (ADDROLE.equals(process)) {
+			addRole();
 		} else if (DATATYPE.equals(process)) {
 			addDataType();
+		} else if (GROUP.equals(process)) {
+			makeGroup();
+		} else if (MATRIX33.equals(process)) {
+			createMatrix33();
+		} else if (NAMEVALUE.equals(process)) {
+			createNameValue();
 		} else if (PULLUP_SINGLETON.equals(process)) {
-			CMLUtil.debug(parsedElement, "PULL");
 			pullupSingleton();
+		} else if (RENAME.equals(process)) {
+			rename();
 		} else if (VECTOR3.equals(process)) {
 			createVector3();
 		}
 	}
 	
+	private void addRole() {
+		if (name == null) {
+			throw new RuntimeException("Must give name");
+		}
+		Nodes names = parsedElement.query(name, CMLConstants.CML_XPATH);
+		for (int i = 0; i < names.size(); i++) {
+			((Element)names.get(i)).addAttribute(new Attribute("role", value));
+		}
+	}
+
+	private void makeGroup() {
+		if (name == null) {
+			throw new RuntimeException("Must give name");
+		}
+		Nodes names = parsedElement.query(name, CMLConstants.CML_XPATH);
+		if (names.size() != 0) {
+			ParentNode parent = names.get(0).getParent();
+			for (int i = 0; i < names.size(); i++) {
+				groupFollowingSiblings(names, parent, i);
+			}
+		}
+	}
+
+	private void groupFollowingSiblings(Nodes names, ParentNode parent, int i) {
+		Element nameElement = (Element)names.get(i);
+		CMLModule module = new CMLModule();
+		module.setRole("group");
+		int index = parent.indexOf(nameElement);
+		parent.insertChild(module, index);
+		index++;
+		int nextIndex = (i == names.size()-1) ? parent.getChildCount() :
+			parent.indexOf((Element)names.get(i+1));
+		for (int j = nextIndex - 1; j >= index; j--) {
+			Node node = parent.getChild(j);
+			if (node == null) {
+				break;
+			}
+			node.detach();
+			module.insertChild(node, 0);
+		}
+	}
+
 	private void pullupSingleton() {
 		if (name == null) {
 			throw new RuntimeException("Must give name");
@@ -127,7 +181,7 @@ public class TransformElement implements MarkupApplier {
 		Nodes names = parsedElement.query(name, CMLConstants.CML_XPATH);
 		for (int i = 0; i < names.size(); i++) {
 			Element nameElement = (Element)names.get(i);
-			CMLUtil.debug(nameElement, "NNNNN");
+//			CMLUtil.debug(nameElement, "NNNNN");
 			Elements childElements = nameElement.getChildElements();
 			if (childElements.size() == 1) {
 				Element child = childElements.get(0);
@@ -150,6 +204,13 @@ public class TransformElement implements MarkupApplier {
     ...
   </list>
 		 */
+		if (parsedElement == null) {
+			throw new RuntimeException("Null parsedElement");
+		}
+		if (parentXpath == null) {
+			CMLUtil.debug(this.element, "DBG");
+			throw new RuntimeException("Null parentXpath "+this.getId());
+		}
 		Nodes parents = parsedElement.query(parentXpath, CMLConstants.CML_XPATH);
 		for (int i = 0; i < parents.size(); i++) {
 			Element parent = (Element)parents.get(i);
@@ -172,6 +233,34 @@ public class TransformElement implements MarkupApplier {
 		valueElement.detach();
 		((CMLScalar)nameElement).setValue(valueValue);
 	}
+
+	private void createMatrix33() {
+		Nodes parents = parsedElement.query(parentXpath, CMLConstants.CML_XPATH);
+		for (int i = 0; i < parents.size(); i++) {
+			Element parent = (Element)parents.get(i);
+			Nodes scalarNodes = parent.query(name, CMLConstants.CML_XPATH);
+			double[] dd = new double[3*3];
+			Node node0 = null;
+			if (scalarNodes.size() == 3*3) {
+				for (i = 0; i < 3*3; i++) {
+					CMLScalar scalar = (CMLScalar) scalarNodes.get(i);
+					dd[i] = scalar.getDouble();
+					if (i == 0) {
+						node0 = scalar;
+					} else {
+						scalar.detach();
+					}
+				}
+				CMLMatrix mat = new CMLMatrix(3, 3, dd);
+				node0.getParent().replaceChild(node0, mat);
+			} else if (scalarNodes.size() == 0) {
+			} else {
+				CMLUtil.debug(parent, "PAR");
+				throw new RuntimeException("No matrix here");
+			}
+		}
+	}
+
 
 	private void createVector3() {
 		/**
@@ -208,7 +297,7 @@ public class TransformElement implements MarkupApplier {
 			}
 		}
 	}
-
+	
 	private void rename() {
 	}
 
