3d6d9f9c42dee693834a0a485b327209451ca1e8
Jens Thomas <9549700+linucks@users.noreply.github.com>
2012-03-10 10:51:53 +0000

    Print the name of the output file to be generated.
    Also trap error if no converter found.

--- jumbo-converters-cli/src/main/java/org/xmlcml/cml/converters/cli/ConverterCli.java
+++ jumbo-converters-cli/src/main/java/org/xmlcml/cml/converters/cli/ConverterCli.java
@@ -14,8 +14,8 @@ import org.xmlcml.euclid.Util;
  */
 public class ConverterCli {
 
-	private static Logger LOG = Logger.getLogger(Converter.class);
-	
+    private static Logger LOG = Logger.getLogger(Converter.class);
+
     public static void main(String[] args) {
 
         if (args.length != 6) {
@@ -29,39 +29,50 @@ public class ConverterCli {
 
             Converter converter = null;
             try {
-            	converter = ConverterRegistry.findConverter(intype, outtype);
+                converter = ConverterRegistry.findConverter(intype, outtype);
+                if (converter == null) {
+                    throw new RuntimeException("Error finding coverter!");
+                    // // usage();
+                }
             } catch (Exception e) {
-            	System.err.println("cannot find converter for: "+Util.concatenate(args, " "));
-            	System.exit(0);
+                System.err.println("cannot find converter for: "
+                        + Util.concatenate(args, " "));
+                System.exit(0);
             }
-            LOG.info("Converter: "+converter);
-//            if (converter == null) {
-////            	usage();
-//            }
-            
+            LOG.info("Converter: " + converter);
+
             File infile = new File(infilename);
             try {
-				LOG.info("input file: "+infile.getAbsolutePath());
-				System.out.println("input file: "+infile.getCanonicalPath());
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw new RuntimeException("Cannot find file: ", e);
-			}
+                LOG.info("input file: " + infile.getAbsolutePath());
+                System.out.println("input file: " + infile.getCanonicalPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Cannot find file: ", e);
+            }
             File outfile = new File(outfilename);
+            try {
+                LOG.info("output file: " + outfile.getAbsolutePath());
+                System.out
+                        .println("output file: " + outfile.getCanonicalPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Cannot open file: ", e);
+            }
 
             converter.convert(infile, outfile);
 
         }
     }
 
-	private static void usage() {
-		System.out.println("Usage:");
-		System.out.println("   -i gau-arc foo.arc -o cml foo.cml");
-		System.out.println();
-		System.out.println("Available converters:");
-		for (ConverterInfo info : ConverterRegistry.getConverterList()) {
-		    System.out.println(info.getInType()+"\t"+info.getOutType()+"\t"+info.getName());
-		}
-	}
+    private static void usage() {
+        System.out.println("Usage:");
+        System.out.println("   -i gau-arc foo.arc -o cml foo.cml");
+        System.out.println();
+        System.out.println("Available converters:");
+        for (ConverterInfo info : ConverterRegistry.getConverterList()) {
+            System.out.println(info.getInType() + "\t" + info.getOutType()
+                    + "\t" + info.getName());
+        }
+    }
 
 }
