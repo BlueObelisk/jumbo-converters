710be35072b6d6f4b366dc123c28be3479a28137
Peter Murray-Rust <pm286@cam.ac.uk>
2014-01-19 10:54:23 +0000

    continued refactoring fragments; started using instance variable; removed many arguments; works on nulcei and ligands but not sprout

---
+++ jumbo-converters-molecule/src/test/java/org/xmlcml/cml/converters/molecule/fragments/FragmentTest.java
@@ -0,0 +1,47 @@
+package org.xmlcml.cml.converters.molecule.fragments;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.xmlcml.cml.base.CMLConstants;
+import org.xmlcml.cml.base.CMLUtil;
+import org.xmlcml.cml.converters.molecule.fragments.ChemistryUtils.CompoundClass;
+import org.xmlcml.cml.element.CMLCml;
+import org.xmlcml.cml.element.CMLMolecule;
+
+public class FragmentTest {
+
+	@Test
+	public void testFragmentsOrganic() {
+		FragmentGenerator fragmentGenerator = new FragmentGenerator();
+		CMLCml cml = (CMLCml) CMLUtil.parseQuietlyIntoCML(new File("src/test/resources/molecule/fragments/2234529.cml"));
+		CMLMolecule molecule = (CMLMolecule) cml.query("//cml:molecule", CMLConstants.CML_XPATH).get(0);
+		fragmentGenerator.processStructure(cml, /*CompoundClass.ORGANIC.toString(), */molecule);
+	}
+	
+	@Test
+	public void testFragmentsInorganic() {
+		FragmentGenerator fragmentGenerator = new FragmentGenerator();
+		CMLCml cml = (CMLCml) CMLUtil.parseQuietlyIntoCML(new File("src/test/resources/molecule/fragments/1008945.cml"));
+		CMLMolecule molecule = (CMLMolecule) cml.query("//cml:molecule", CMLConstants.CML_XPATH).get(0);
+		fragmentGenerator.processStructure(cml, /*CompoundClass.ORGANIC.toString(),*/ molecule);
+	}
+	
+	@Test
+	public void testFragmentsMetal() {
+		FragmentGenerator fragmentGenerator = new FragmentGenerator();
+		CMLCml cml = (CMLCml) CMLUtil.parseQuietlyIntoCML(new File("src/test/resources/molecule/fragments/metal.cml"));
+		CMLMolecule molecule = (CMLMolecule) cml.query("//cml:molecule", CMLConstants.CML_XPATH).get(0);
+		fragmentGenerator.processStructure(cml, /*CompoundClass.ORGANIC.toString(),*/ molecule);
+	}
+	
+	@Test
+	public void testFragmentsMetal1() {
+		FragmentGenerator fragmentGenerator = new FragmentGenerator();
+		CMLCml cml = (CMLCml) CMLUtil.parseQuietlyIntoCML(new File("src/test/resources/molecule/fragments/mbok1z.cml"));
+		CMLMolecule molecule = (CMLMolecule) cml.query("//cml:molecule", CMLConstants.CML_XPATH).get(0);
+		fragmentGenerator.processStructure(cml, /*CompoundClass.ORGANIC.toString(),*/ molecule);
+	}
+	
+	
+}
