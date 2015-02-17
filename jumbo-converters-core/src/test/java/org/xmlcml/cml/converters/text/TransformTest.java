56ccd36f22846e53e8f939081707611ee9dd3c33
Oliver Stueker <ostueker@mun.ca>
2015-02-17 16:03:49 -0310

    core: added test for upcoming transform createTriangularMatrix

--- jumbo-converters-core/src/test/java/org/xmlcml/cml/converters/text/TransformTest.java
+++ jumbo-converters-core/src/test/java/org/xmlcml/cml/converters/text/TransformTest.java
@@ -705,6 +705,27 @@ public class TransformTest {
 			);
 	}
 	
+	@Ignore
+	@Test
+	public void testCreateTriangularMatrix(){
+		String listXML =
+			"<list dictRef='x:fullMatrix' >"+
+			"  <array dataType='xsd:double' size='5'>0.0 0.1 0.2 0.3 0.4</array>"+
+			"  <array dataType='xsd:double' size='5'>1.0 1.1 1.2 1.3 1.4</array>"+
+			"  <array dataType='xsd:double' size='5'>2.0 2.1 2.2 2.3 2.4</array>"+
+			"  <array dataType='xsd:double' size='5'>3.0 3.1 3.2 3.3 3.4</array>"+
+			"  <array dataType='xsd:double' size='5'>4.0 4.1 4.2 4.3 4.4</array>"+
+			"</list>" ;
+		String transformXML = "<transform process='createTriangularMatrix' xpath='.' from='.//cml:list[@dictRef=\"x:fullMatrix\"]' />" ;
+		String arrayXML = "<array>0.0 1.0 1.1 2.0 2.1 2.2 3.0 3.1 3.2 3.3 4.0 4.1 4.2 4.3 4.4</array>" ;
+
+		runTest("createTriangularMatrix",
+			transformXML,
+			listXML,
+			arrayXML
+			);
+	}
+
 	@Test 
 	public void testCreateVector3() {
 		CMLList list = new CMLList();
