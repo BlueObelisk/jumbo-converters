d9ccbb2a1d0909d2ca09b84936bfd88c2bd91fc7
Oliver Stueker <ostueker@users.noreply.github.com>
2015-06-08 17:33:29 -0210

    QCHEM: added several draft templates

--- jumbo-converters-compchem/jumbo-converters-compchem-qchem/src/test/java/org/xmlcml/cml/converters/compchem/qchem/log/TemplateTest.java
+++ jumbo-converters-compchem/jumbo-converters-compchem-qchem/src/test/java/org/xmlcml/cml/converters/compchem/qchem/log/TemplateTest.java
@@ -35,13 +35,29 @@ public class TemplateTest {
 	}
 
 	////////////  //////////// ADD TEMPLATE TESTS HERE: ////////////  ////////////
-	@Test   public void testStdNuclOrient()          {runTemplateTest("standard.nuclear.orientation");}
+	@Test   public void testEnvironPlatformOld()	{runTemplateTest("env.platform");}
+	@Test   public void testEnvironPlatformNew()	{runTemplateTest("env.platform.new");}
+	@Test   public void testEnvironVersion()		{runTemplateTest("env.version");}
+	@Test   public void testStdNuclOrient()			{runTemplateTest("standard.nuclear.orientation");}
 
 
 	////////////  //////////// READY TO WORK ON: ////////////  ////////////
+	@Ignore @Test public void testCoordsAngstoms()			{runTemplateTest("coordinates.angstoms");}
+	@Ignore @Test public void testDistanceMatrix()			{runTemplateTest("distance.matrix");}
+	@Ignore @Test public void testEnvironHostname()			{runTemplateTest("env.hostname");}
+	@Ignore @Test public void testEnvironTimetamp()			{runTemplateTest("env.timestamp");}
+	@Ignore @Test public void testFreqForceConstMatrix()	{runTemplateTest("freq.forceConstantMatrix");}
+	@Ignore @Test public void testFreqVibAnalalysis()		{runTemplateTest("freq.vib.analysis");}
+	@Ignore @Test public void testGradients()				{runTemplateTest("gradients");}
+	@Ignore @Test public void testInitBasis()				{runTemplateTest("init.basis");}
+	@Ignore @Test public void testMullikenCharges()			{runTemplateTest("mulliken.charges");}
+	@Ignore @Test public void testMultipoleMoments()		{runTemplateTest("multipole.moments");}
+	@Ignore @Test public void testPolarizabilityMatrix()	{runTemplateTest("polarizability.matrix");}
+	@Ignore @Test public void testThermoChemistry()			{runTemplateTest("thermochemistry");}
 
 
 	////////////  //////////// KEEP FOR LATER: ////////////  ////////////
+	@Ignore @Test   public void testInitUserInput()			{runTemplateTest("init.userInput");}
 
 
 	////////////  //////////// END OF TEMPLATE TESTS ////////////  ////////////
