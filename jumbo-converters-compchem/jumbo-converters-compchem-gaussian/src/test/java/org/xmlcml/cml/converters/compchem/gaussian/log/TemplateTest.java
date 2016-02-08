02d5bf53ac35721faa46d3b4bfdfefd9ff3a7af6
Kelwin Joanes <kjoanes@gmail.com>
2016-02-08 18:00:19 -0430

    GAUSSIAN: modified thermochemistry module, renamed cc:zpe to cc:Energy_ZPVE, fixed l716.xml test and added new frequencyAnalysis module to finalization module

--- jumbo-converters-compchem/jumbo-converters-compchem-gaussian/src/test/java/org/xmlcml/cml/converters/compchem/gaussian/log/TemplateTest.java
+++ jumbo-converters-compchem/jumbo-converters-compchem-gaussian/src/test/java/org/xmlcml/cml/converters/compchem/gaussian/log/TemplateTest.java
@@ -93,7 +93,7 @@ public class TemplateTest {
 			@Test	public void testl701()                       {runTemplateTest("l701");}
 			@Test	public void testl702()                       {runTemplateTest("l702");}
 			@Test	public void testl703()                       {runTemplateTest("l703");}
-	@Ignore @Test	public void testl716()                       {runTemplateTest("l716");}
+			@Test	public void testl716()                       {runTemplateTest("l716");}
 
 			@Test	public void testl716Diagvib()                {runTemplateTest("l716/", "l716.diagvib");}
 			@Test	public void testl716Dipole()                 {runTemplateTest("l716/", "l716.dipole");}
